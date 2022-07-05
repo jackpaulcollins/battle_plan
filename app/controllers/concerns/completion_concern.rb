aa44f5f15dd971991453dabe0d5ec02dcb7fa002
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-07-05 15:00:37 -0700

    convert param to date object before passing to method

--- app/controllers/concerns/completion_concern.rb
+++ app/controllers/concerns/completion_concern.rb
@@ -5,12 +5,14 @@ module CompletionConcern
 
   def does_completion_complete_plan_for_day?(plan_id, day)
     plan = Plan.find(plan_id)
+    day = Date.parse(day)
     plan.tasks.all? { |task| task.completed_for_day?(day) }
   end
 
   def does_destroying_completion_incomplete_plan_for_day?(plan_id, day)
     return if !Completion.where(completable_type: "Plan", completable_id: plan_id, completed_on: day).exists?
     plan = Plan.find(plan_id)
+    day = Date.parse(day)
     !plan.tasks.all? { |task| task.completed_for_day?(day) }
   end
 end
